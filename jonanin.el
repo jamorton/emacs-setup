3817b4313862d4be7057adbf2702f07cefce636b
Jon Morton <jonanin@gmail.com>
2011-09-19 18:10:42 -0500

    add js2-mode configs

--- jonanin.el
+++ jonanin.el
@@ -79,7 +79,10 @@
 ;; Javascript mode
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
+(add-hook 'js2-mode-hook
+  '(lambda() (
+    (set js2-pretty-multiline-decl-indentation-p t)
+    (set js2-consistent-level-indent-inner-bracket-p t))))
 
 ;; the end
 (provide 'jonanin)
