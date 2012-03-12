a820626749a838b17467ad3f16ae6d454c627885
Jon Morton <jonanin@gmail.com>
2012-03-11 20:19:18 -0500

    add additional ruby types

--- jonanin-ruby.el
+++ jonanin-ruby.el
@@ -1,4 +1,10 @@
 
+(add-to-list 'auto-mode-alist '("\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
 (add-hook 'ruby-mode-hook
   (lambda ()
 	(set-local-key (kbd "RET") 'newline-and-indent)
