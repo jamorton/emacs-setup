bd7249578da0c69970a265a4a825370433034bfa
Jon Morton <jonanin@gmail.com>
2014-01-13 15:07:32 -0600

    Interpret Vagrantfiles as ruby

--- modes/ruby.el
+++ modes/ruby.el
@@ -4,6 +4,7 @@
 (add-to-list 'auto-mode-alist '("\.ru$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 
 (add-hook 'ruby-mode-hook
   (lambda ()
