30cbd6f93ea41000f351f397ff82b13540861e57
Jon Morton <jonanin@gmail.com>
2014-05-27 19:43:16 -0700

    Create intermediate directories before saving files

--- jonanin.el
+++ jonanin.el
@@ -19,5 +19,14 @@
     (load-theme 'zenburn t)
   ))
 
+;; create intermediate directories before saving a file
+(add-hook 'before-save-hook
+          (lambda ()
+            (when buffer-file-name
+              (let ((dir (file-name-directory buffer-file-name)))
+                (when (and (not (file-exists-p dir))
+                           (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
+                  (make-directory dir t))))))
+
 ;; the end
 (provide 'jonanin)
