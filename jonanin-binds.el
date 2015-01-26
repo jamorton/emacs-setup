7093c1583fc9c730bda93e89b972673979e06cfe
Jon Morton <jonanin@gmail.com>
2015-01-26 01:51:51 -0600

    Update .gitignore

--- jonanin-binds.el
+++ jonanin-binds.el
@@ -8,6 +8,12 @@
 ;; Insert on each line
 (global-set-key (kbd "C-x r a") 'string-insert-rectangle)
 
+;; Smex
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
 (setq compilation-read-command nil)
 (setq compilation-ask-about-save nil)
 (global-set-key (kbd "C-x c") 'compile)
